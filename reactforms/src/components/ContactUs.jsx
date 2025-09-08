ce0872ee67beeddcc6460d99996006b4fe003cb0
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-08 19:13:48 +0530

    initial login page

--- reactforms/src/components/ContactUs.jsx
+++ reactforms/src/components/ContactUs.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function ContactUs() {
   return (
     <div> 
-        
+        <form>
+            <label> 
+                first Name : 
+                <input type="text"  
+                 name='firstname'
+                 value={''}/>
+            </label> 
+
+            <label>
+                last Name : 
+                <input type="text"
+                name='lastname'
+                value={''} />
+            </label>  
+
+            <label>
+              <input type="text" 
+              name = 'email' 
+              value = {''} />
+            </label> 
+
+            <label>
+              <input type="text"
+              name = 'password'
+              value={''} />
+            </label>
+            <p>first name : {} , last name : {}</p>
+        </form>
     </div>
   )
 }
