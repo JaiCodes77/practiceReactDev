0d4f0dd924691ec11a1d586cf282bd42286fca93
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-09 11:43:51 +0530

    styling the form

--- reactforms/src/components/ContactUs.jsx
+++ reactforms/src/components/ContactUs.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './ContactUs.css';
 
 function ContactUs() { 
 
@@ -16,7 +17,7 @@ function ContactUs() {
   } 
 
   return (
-    <div> 
+    <div className='containerdiv'> 
         <form onSubmit={handleSubmit}> 
             <label> 
                 first Name : 
