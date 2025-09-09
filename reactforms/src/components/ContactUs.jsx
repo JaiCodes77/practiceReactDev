2383d2aa7bc97d5f75bf6b03886491bc5d1d039a
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-09 12:34:48 +0530

    added initial css

--- reactforms/src/components/ContactUs.jsx
+++ reactforms/src/components/ContactUs.jsx
@@ -1,60 +1,74 @@
-import React, { useState } from 'react'
-import './ContactUs.css';
+import React, { useState } from "react";
+import "./ContactUs.css";
 
-function ContactUs() { 
-
+function ContactUs() {
   const [inputs, setInputs] = useState({});
 
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
     alert(JSON.stringify(inputs));
   }
 
-  function handleChange(e){
+  function handleChange(e) {
     const name = e.target.name;
     const value = e.target.value;
-    setInputs(values=>({...values, [name]: value}))
-  } 
+    setInputs((values) => ({ ...values, [name]: value }));
+  }
 
-  return (
-    <div className='containerdiv'> 
-        <form onSubmit={handleSubmit}> 
-            <label> 
-                first Name : 
-                <input type="text"  
-                 name='firstname'
-                 value={inputs.firstname || ''}
-                 onChange={handleChange}/>
-            </label> 
+  return ( 
+    <div className="containerdiv"> 
+    <div className="containerstylingdiv"> 
+      <form onSubmit={handleSubmit} className="formdiv">
+        <div className="labeldiv"> 
+          <label>first Name :</label>
+          <input
+            type="text"
+            name="firstname"
+            value={inputs.firstname || ""}
+            onChange={handleChange}
+            className="inputfield"
+          />
+        </div>
 
-            <label>
-                last Name : 
-                <input type="text"
-                name='lastname'
-                value={inputs.lastname || ''}
-                onChange={handleChange} />
-            </label>  
+        <div className="labeldiv">
+          <label>last Name :</label>
+          <input
+            type="text"
+            name="lastname"
+            value={inputs.lastname || ""}
+            onChange={handleChange}
+            className="inputfield"
+          />
+        </div>
 
-            <label> 
-              Email : 
-              <input type="email" 
-              name = 'email' 
-              value = {inputs.email || ''}
-              onChange={handleChange} />
-            </label> 
+        <div className="labeldiv">
+          <label>Email :</label>
+          <input
+            type="email"
+            name="email"
+            value={inputs.email || ""}
+            onChange={handleChange}
+            className="inputfield"
+          />
+        </div>
 
-            <label> 
-              Password : 
-              <input type="password"
-              name = 'password'
-              value={inputs.password || ''}
-              onChange={handleChange}/> 
-            </label> 
+        <div className="labeldiv">
+          {" "}
+          <label>Password :</label>
+          <input
+            type="password"
+            name="password"
+            value={inputs.password || ""}
+            onChange={handleChange}
+            className="inputfield"
+          />
+        </div>
 
-            <button type='submit'>Submit</button>
-        </form>
+        <button type="submit">Submit</button>
+      </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
