88d4a2272872284f0ba7d5d8c0a38cc291c4b3e7
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-15 16:00:07 +0530

    bug fixed

--- routing/src/components/Navbar.jsx
+++ routing/src/components/Navbar.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import {NavLink } from 'react-router-dom'
-import './Navbar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className='containerdiv'> 
-      <div className='sidebar'> 
-        <div className='sidebarcontent'>
-       <button><NavLink to='/'>Home</NavLink></button>
-      <button><NavLink to='/about'>About</NavLink></button>
-      <button><NavLink to='/contact'>Contact</NavLink></button> 
-      <button><NavLink to='/dashboard'>Dashboard</NavLink></button>
+    <div className="containerdiv">
+      <div className="sidebar">
+        <div className="sidebarcontent">
+          <button>
+            <NavLink
+              to="/"
+              className={({isActive}) => (isActive ? "active-link" : " ")}
+            >
+              Home
+            </NavLink>
+          </button>
+          <button>
+            <NavLink
+              to="/about"
+              className={({isActive}) => (isActive ? "active-link" : "")}
+            >
+              About
+            </NavLink>
+          </button>
+          <button>
+            <NavLink
+              to="/contact"
+              className={({isActive}) => (isActive ? "active-link" : " ")}
+            >
+              Contact
+            </NavLink>
+          </button>
+          <button>
+            <NavLink
+              to="/dashboard"
+              className={({isActive}) => (isActive ? "active-link" : " ")}
+            >
+              Dashboard
+            </NavLink>
+          </button>
+        </div>
       </div>
-      </div> 
-      <div className='maincontent'> 
-       
-      </div>
+      <div className="maincontent"></div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
