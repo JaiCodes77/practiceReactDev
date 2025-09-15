22966db09fa44d468a1bd247df0de2117a9da4a3
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-15 11:30:29 +0530

    initial commit

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
