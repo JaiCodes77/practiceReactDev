794618a6b69099a097f38d596382c74314e028e5
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-15 15:40:16 +0530

    done with routing setup

--- routing/src/components/Navbar.jsx
+++ routing/src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {NavLink } from 'react-router-dom'
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>
+    <div className='containerdiv'> 
+      <div className='sidebar'> 
+        <div className='sidebarcontent'>
        <button><NavLink to='/'>Home</NavLink></button>
       <button><NavLink to='/about'>About</NavLink></button>
       <button><NavLink to='/contact'>Contact</NavLink></button> 
       <button><NavLink to='/dashboard'>Dashboard</NavLink></button>
+      </div>
+      </div> 
+      <div className='maincontent'> 
+       
+      </div>
     </div>
   )
 }
--- routing/src/pages/Dashboard.jsx
+++ routing/src/pages/Dashboard.jsx
@@ -4,7 +4,6 @@ import { NavLink, Outlet } from 'react-router-dom'
 function Dashboard() {
   return (
     <div>
-        Reports
         <button><NavLink to='reports'>Reports</NavLink></button> 
         <Outlet/>
     </div>
