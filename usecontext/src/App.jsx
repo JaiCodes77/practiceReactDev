8e04788db6ea9ddec72e9baa0f92da219d8c6bbc
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-16 15:40:12 +0530

    shopping cart

--- usecontext/src/App.jsx
+++ usecontext/src/App.jsx
@@ -4,18 +4,23 @@ import ChildA from './components/ChildA';
 import { useState } from 'react';
 
 const UserContext = createContext(); 
-const ThemeContext = createContext();
+const ThemeContext = createContext(); 
+const CartContext = createContext();
 function App() { 
   const [user,setUser] = useState({name : 'Jai', age : 21}) 
-  const [theme,setTheme] = useState('light');
+  const [theme,setTheme] = useState('light'); 
+
+  const [cart,setCart] = useState([{name : "haier", price : 2400}, {name : "huwawei", price : 4000}])
   return (
     <div> 
-      <UserContext.Provider value={user}>
+      <UserContext.Provider value={user}> 
+        <CartContext.Provider value={{cart,setCart}}>
         <ThemeContext.Provider value={{theme,setTheme}}>
           <div id='container' style={{backgroundColor : theme==='light' ? 'beige' : 'black'}}>
             <ChildA/>
           </div>
-        </ThemeContext.Provider>
+        </ThemeContext.Provider> 
+        </CartContext.Provider>
       </UserContext.Provider>
     </div>
   )
@@ -23,4 +28,5 @@ function App() {
 
 export default App 
 export {UserContext} 
-export {ThemeContext}
+export {ThemeContext} 
+export {CartContext}
--- usecontext/src/components/ChildC.jsx
+++ usecontext/src/components/ChildC.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { UserContext,ThemeContext } from '../App'
+import { UserContext,ThemeContext, CartContext } from '../App'
 import { useContext } from 'react'
 
 function ChildC() {
     const user = useContext(UserContext);
-    const {theme,setTheme}= useContext(ThemeContext); 
+    const {theme,setTheme}= useContext(ThemeContext);  
+    const {cart,setCart} = useContext(CartContext);
 
     function togglecolor(){
         if(theme==='light'){
@@ -12,12 +13,16 @@ function ChildC() {
         } else {
             setTheme('light');
         }
-    }
+    } 
+
+    
   return (
-    <div>ChildC 
+    <div>ChildC  
         <button onClick={togglecolor}>Change Theme</button>
          <p>data : {user.name}</p>
-         <p> data 2 : {user.age}</p>
+         <p> data 2 : {user.age}</p> 
+
+         <h3> shopping cart : {cart[0].name}-{cart[0].price}</h3>
     </div>
   )
 }
