df1ca6e194c9fcb84bd10bc6a9f1d0134b216791
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-16 14:51:27 +0530

    used multiple contexts

--- usecontext/src/App.jsx
+++ usecontext/src/App.jsx
@@ -4,16 +4,23 @@ import ChildA from './components/ChildA';
 import { useState } from 'react';
 
 const UserContext = createContext(); 
+const ThemeContext = createContext();
 function App() { 
-  const [user,setUser] = useState({name : 'Jai', age : 21})
+  const [user,setUser] = useState({name : 'Jai', age : 21}) 
+  const [theme,setTheme] = useState('light');
   return (
     <div> 
       <UserContext.Provider value={user}>
-        <ChildA/>
+        <ThemeContext.Provider value={{theme,setTheme}}>
+          <div id='container' style={{backgroundColor : theme==='light' ? 'beige' : 'black'}}>
+            <ChildA/>
+          </div>
+        </ThemeContext.Provider>
       </UserContext.Provider>
     </div>
   )
 }
 
 export default App 
-export {UserContext}
+export {UserContext} 
+export {ThemeContext}
--- usecontext/src/components/ChildC.jsx
+++ usecontext/src/components/ChildC.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { UserContext } from '../App'
+import { UserContext,ThemeContext } from '../App'
 import { useContext } from 'react'
 
 function ChildC() {
     const user = useContext(UserContext);
+    const {theme,setTheme}= useContext(ThemeContext); 
+
+    function togglecolor(){
+        if(theme==='light'){
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }
   return (
-    <div>ChildC
+    <div>ChildC 
+        <button onClick={togglecolor}>Change Theme</button>
          <p>data : {user.name}</p>
          <p> data 2 : {user.age}</p>
     </div>
