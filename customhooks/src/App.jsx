172909206d3b8380e965f9c2a69038a08bbf255a
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-17 12:37:29 +0530

    useCurrency hook is working

--- customhooks/src/App.jsx
+++ customhooks/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import useCurrency from './hooks/useCurrency'
 
-function App() {
+function App() { 
+  const currencyInfo = useCurrency("btc");
+  const options = Object.keys(currencyInfo);
   return (
-    <div>App</div>
+    <div>
+      <h3>Currency Info: </h3>
+      <p>{JSON.stringify(currencyInfo)}</p>  
+
+      <h4>Currency Options: </h4>
+      <ul>
+        {options.map(id => (
+          <li key={id}>{id}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
