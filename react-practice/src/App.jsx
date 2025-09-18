45dfe15074b7250aad9c8d6daeabf3abb2ca280f
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-19 00:09:27 +0530

    commit

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='containerdiv'>
+     <div className="item item-1">Item1</div>
+     <div className="item item-2">Item2</div>
+     <div className="item item-3">Item3</div>
+     <div className="item item-4">Item4</div>
+     <div className="item item-5">Item5</div>
+   </div>
+  )
+}
+
+export default App
