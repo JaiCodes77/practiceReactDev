7f2fdd1cc33fb4ecd9c3f82f300e96410a0ab2d1
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-18 12:22:01 +0530

    movieapp using localstoragehook

--- localstoragehook/src/App.jsx
+++ localstoragehook/src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
-  return (
-    <div> 
-      App
-    </div>
-  )
+    const [movies, setMovies] = useLocalStorage('movies', []);
+    const [movieName, setMovieName] = useState('');
+
+    const addMovie = () => {
+        if (movieName.trim() !== '') {
+            setMovies([...movies, movieName.trim()]);
+            setMovieName('');
+        }
+    };
+
+    const removeMovie = (nameToRemove) => {
+        setMovies(movies.filter(movie => movie !== nameToRemove));
+    };
+
+    return (
+        <div>
+            <h1>Favorite Movies</h1>
+
+            <input
+                type="text"
+                value={movieName}
+                onChange={(e) => setMovieName(e.target.value)}
+                placeholder="Add a new movie"
+            />
+            <button onClick={addMovie}>Add</button>
+
+            <ul>
+                {movies.map((movie, index) => (
+                    <li key={index}>
+                        {movie}
+                        <button onClick={() => removeMovie(movie)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- localstoragehook/src/useLocalStorage.js
+++ localstoragehook/src/useLocalStorage.js
@@ -1,5 +1,16 @@
 import { useEffect,useState } from "react";
 
-function useLocalStorage(){
+function useLocalStorage(key,initialValue){
+    const [value,setValue] = useState(()=>{
+        const stored = localStorage.getItem(key);
+        return stored ? JSON.parse(stored) : initialValue;
+    });
+
+    useEffect(() => {
+     localStorage.setItem(key,JSON.stringify(value));
+    }, [key,value])
     
-}
+    return [value,setValue];
+} 
+
+export default useLocalStorage;
