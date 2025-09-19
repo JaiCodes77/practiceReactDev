4435aa7ee7ecfeb48a8ec932121633d7738ab96d
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-19 11:38:07 +0530

    customhooks updation

--- customhooks/src/hooks/useCurrency.js
+++ customhooks/src/hooks/useCurrency.js
@@ -8,7 +8,8 @@ function useCurrency(currency){
      fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
      .then((res)=> res.json())
      .then((res)=>setData(res[currency]))
-   }, [currency])
+   }, [currency]) 
+   
    console.log(data)
    return data;
 } 
