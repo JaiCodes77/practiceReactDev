24cf80584e21618f0d4d649815b7b2994dc6ddc7
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-19 15:33:16 +0530

    example usage of useref

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useRef } from 'react';
+import { useState } from 'react'
 
-function App() {
+function App() { 
+
+  const [count,setCount] = useState(0); 
+  
+  let val = useRef(0);
+
+  function handleIncrement(){ 
+    val.current = val.current + 1;
+    setCount(count+1);
+  }
   return (
-    <div>App</div>
+    <div>App 
+
+      <button onClick={handleIncrement}>Increment</button> 
+
+      <h4>count is : {count}</h4>
+      <h5> val is : {val.current}</h5>
+    </div>
   )
 }
 
