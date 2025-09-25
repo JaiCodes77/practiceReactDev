e95ae046abff2e71afbbc00504c791323bb5fa80
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-25 22:44:27 +0530

    defined the endpoints

---
+++ rtk-query/src/services/JsonPlaceholderApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const jsonPlaceholderApi = createApi({
+  reducerPath: "jsonPlaceholderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com/",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({ query: () => "Posts" }),
+    getPostById: builder.query({ query: (id) => `Posts/${id}` }),
+    addPost: builder.mutation({
+      query: (newPost) => ({
+        url: "posts",
+        method: "POST",
+        body: newPost,
+      }),
+    }),
+  }),
+});
+
+export const { useGetPostsQuery, useGetPostByIdQuery, useAddPostMutation } =
+  jsonPlaceholderApi;
