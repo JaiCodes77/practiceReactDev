691a2ba00a624e2a0be07078836eb179e6dd975c
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-26 12:18:47 +0530

    rtk query practice done

--- rtk-query/src/App.jsx
+++ rtk-query/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react' 
+import { useGetPostByIdQuery,useGetPostsQuery,useAddPostMutation } from './services/JsonPlaceHolderApi'
 
-function App() {
+function App() { 
+  const {data : posts,error,isLoading} = useGetPostsQuery();
+  const {data: singlePost} = useGetPostByIdQuery(1);
+  const [addPost] = useAddPostMutation();
+
+  if(isLoading) return <p>...Loading</p>;
+  if(error) return <p>...error</p>;
+
   return (
-    <div>App</div>
+    <div>
+      <h2>RTK Query Practice</h2>
+      <h3>All posts</h3>
+      {posts && posts.slice(0,5).map(post=>(<li key={post.id}>{post.title}</li>))}
+
+      <h3>singlePost</h3>
+      {singlePost && <p>{singlePost.title}</p>}
+
+       <button 
+        onClick={() => addPost({ title: 'Hello RTK Query', body: 'Learning...', userId: 1 })}
+      >
+        Add Post
+      </button>
+    </div>
   )
 }
 
