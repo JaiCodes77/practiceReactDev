c7d1d986d6ff74d9797fe58306c5350f405f5a9a
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-05 21:51:40 +0530

    api called

--- tailwind-practice/src/components/Dashboard.jsx
+++ tailwind-practice/src/components/Dashboard.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Dashboard() {
-  return (
-    <div className='min-h-screen min-w-screen'>
-        <h2 className='font-bold text-3xl m-8'>Overview</h2>
-        <div> 
+  const [data, setData] = useState();
 
-            <div className='flex justify-evenly mr-90'> 
-                <h3 className='font-bold text-xl'>Portfolio</h3>
-                <h3 className='font-bold text-xl'>Your Assets</h3>
-            </div> 
-            <div className='flex gap-4 justify-center items-center'>
-            <div className='bg-sky-200 w-[550px] h-[300px] flex justify-center items-center mt-10 rounded-xl'>$17,000</div>
-            <div className='bg-purple-200 w-[250px] h-[300px] mt-10 rounded-xl flex justify-center items-center'>125 BTC</div>
-            <div className='bg-green-200 w-[250px] h-[300px] mt-10 rounded-xl flex justify-center items-center'>155 ETH</div>
-            <div  className='bg-yellow-200 w-[250px] h-[300px] mt-10 rounded-xl flex justify-center items-center'>400 BNB</div>
-            </div>
+  useEffect(() => {
+    const api = () =>
+      fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd`
+      )
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((error) => console.log(error));
+
+    api();
+  }, []);
+
+  return (
+    <div className="min-h-screen min-w-screen">
+      <h2 className="font-bold text-3xl m-8">Overview</h2>
+      <div>
+        <div className="flex justify-evenly mr-90">
+          <h3 className="font-bold text-xl">Portfolio</h3>
+          <h3 className="font-bold text-xl">Your Assets</h3>
         </div>
+        <div className="flex gap-4 justify-center items-center">
+          <div className="bg-sky-200 w-[550px] h-[300px] flex justify-center items-center mt-10 rounded-xl">
+            $17,000
+          </div>
+          <div className="bg-purple-200 w-[250px] h-[300px] mt-10 rounded-xl flex justify-center items-center">
+            125 BTC
+          </div>
+          <div className="bg-green-200 w-[250px] h-[300px] mt-10 rounded-xl flex justify-center items-center">
+            155 ETH
+          </div>
+          <div className="bg-yellow-200 w-[250px] h-[300px] mt-10 rounded-xl flex justify-center items-center">
+           {data?.map(d => <li key={d.id}>{d.name}</li>)}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-end">
+        <div className="bg-black w-[450px] h-[400px] rounded-xl mt-10 mr-11"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
