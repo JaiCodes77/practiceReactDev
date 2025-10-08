d2bce5d1d6927640fcb5db8b1d780d16f34529c4
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-08 14:44:51 +0530

    basicc styling done

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <div></div>
+    <div className='bg-fuchsia-900 h-screen flex justify-center items-center'>
+      <div className='bg-fuchsia-900 w-[1200px] h-[600px] flex mb-20 rounded-lg shadow-xl/40'>
+      <h2 className='text-white font-black text-5xl mt-10 ml-20'>Li</h2> 
+      <h2 className='text-white font-black text-7xl mt-45'>Welcome!</h2>
+      </div>
     </div>
   )
 }
