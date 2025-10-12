df64c6ae834a173fa3f82150b71701d60d27ef39
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-12 19:33:52 +0530

    somewhat finalized

--- rtk-query-prac/src/App.jsx
+++ rtk-query-prac/src/App.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react' 
+import {useGetPostsQuery,useCreatePostsMutation} from './services/JsonPlaceholderApi';
 
-function App() {
+function App() { 
+  const {data,error,isLoading} = useGetPostsQuery();
+
+  if(isLoading) return <h3>Loading...</h3> ; 
+  if(error) return <h3>error...</h3> ;
+
+  console.log(data)
+
   return (
     <div>App</div>
   )
