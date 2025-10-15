225c815ac3c2af70b7b36ccbc7dd29dee10e5e07
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-15 14:49:59 +0530

    demo app completed for testing

--- my-testing-app/src/App.js
+++ my-testing-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+function App() { 
+  const [count,setCount] = useState(0)
+
+  function increment(){
+    setCount(count+1)
+  } 
+
+  function decrement(){
+    setCount(count-1)
+  } 
+
+  function reset(){
+    setCount(0)
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
