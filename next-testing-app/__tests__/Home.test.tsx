63fb831955459e126521a774a4fd909cccc1f207
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-22 16:47:58 +0530

    adding more sample test cases

--- next-testing-app/__tests__/Home.test.tsx
+++ next-testing-app/__tests__/Home.test.tsx
@@ -1,10 +1,29 @@
 import {render,screen} from '@testing-library/react'
 import Home from '@/app/page'
+import { describe } from 'node:test'
 
+describe('Home',()=>{
 it('should have Docs text', () => {
     render(<Home/>) //ARRANGE 
 
     const myElem = screen.getByText('Read our docs') //ACT
 
     expect(myElem).toBeInTheDocument()//ASSERT
+})
+
+it('should have the text "instantly"', () => {
+    render(<Home/>) //ARRANGE 
+
+    const myElem = screen.getByText(/instantly/i) //ACT
+
+    expect(myElem).toBeInTheDocument()//ASSERT
+})
+
+it('list', () => {
+    render(<Home/>) //ARRANGE 
+
+    const myElem = screen.getByRole('list') //ACT
+
+    expect(myElem).toBeInTheDocument()//ASSERT
+})
 })
