eb48de4e22335473993a59517e90dfb749f1e60e
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-24 12:43:00 +0530

    formatted

--- next-testing-app/src/app/page.tsx
+++ next-testing-app/src/app/page.tsx
@@ -1,20 +1,22 @@
-import AddTodo from '@/components/AddTodo/AddTodo'
-import Header from '@/components/Header/Header'
-import TodoItem from '@/components/TodoItem/TodoItem'
-import React from 'react'
+import AddTodo from "@/components/AddTodo/AddTodo";
+import Header from "@/components/Header/Header";
+import TodoItem from "@/components/TodoItem/TodoItem";
+import React from "react";
 
-
-type Props = {}
+type Props = {};
 
 const page = (props: Props) => {
-  const [todo, setTodo] = React.useState<string>('');
-  
-  return (
-    <div className='flex flex-col gap-6'><Header/>
-   <div className='flex flex-row justify-center items-center gap-2'><TodoItem/>
-    <AddTodo/></div>
-    </div>
-  )
-}
+  const [todo, setTodo] = React.useState<string>("");
 
-export default page
+  return (
+    <div className="flex flex-col gap-6">
+      <Header />
+      <div className="flex flex-row justify-center items-center gap-2">
+        <TodoItem />
+        <AddTodo />
+      </div>
+    </div>
+  );
+};
+
+export default page;
