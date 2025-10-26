bbdd92dbb1b9c10b4873e6be77951c196c4b8c63
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-26 12:33:11 +0530

    fixed unused import

--- next-testing-app/src/components/Header/Header.test.tsx
+++ next-testing-app/src/components/Header/Header.test.tsx
@@ -1,6 +1,5 @@
 import {render,screen} from '@testing-library/react';
 import Header from './Header';
-import Head from 'next/head';
 
 describe('header component',()=>{
     it('should render header component',()=>{
