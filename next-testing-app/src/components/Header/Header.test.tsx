f9ed51a6c265312d6b9e7e5b7647a7ab56dcba3b
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-10-26 12:34:57 +0530

    added comments for better understandability

--- next-testing-app/src/components/Header/Header.test.tsx
+++ next-testing-app/src/components/Header/Header.test.tsx
@@ -4,19 +4,19 @@ import Header from './Header';
 describe('header component',()=>{
     it('should render header component',()=>{
         render(<Header/>);
-    }) 
+    }) //checks whether the component is rendering or not(passed)
 
     it('displays the app title', () => {
         render(<Header/>); 
 
         const title = screen.getByText('My Todo App');
         expect(title).toBeInTheDocument();
-    }) 
+    }) // checks whether the title is in the page or not(passed) 
 
     it('renders title as a heading for accessibility', () => {
         render(<Header/>); 
 
         const title = screen.getByRole('heading');
         expect(title).toBeInTheDocument();
-    })
+    }) //checks whether the heading tag is being used in the component or not(passed)
 })
