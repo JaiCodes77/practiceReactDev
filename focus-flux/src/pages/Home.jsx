b74dff5fe005d9f35848c24d69f6a1025cc5eb71
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-11-07 20:46:25 +0530

    making the home page

--- focus-flux/src/pages/Home.jsx
+++ focus-flux/src/pages/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className=''>Home</div>
+    <div className='h-screen w-screen bg-purple-400 fixed top-0 left-64 flex justify-center items-center'>
+      <div className='mb-80'>
+      <div className='h-100 w-160 bg-purple-300 mr-84 rounded-lg shadow-xl/30 flex'>
+      <div className='flex space-between'>
+      <button>focus time</button>
+      <button>short break</button>
+      <button>long break</button>
+      </div>
+      </div>
+      </div>
+      </div>
   )
 }
 
